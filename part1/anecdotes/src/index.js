eba3fb76d472a599c9bb5cd514fd638e7e89e28e
Jidmaa <majidbenhenneda@gmail.com>
2020-09-20 11:31:41 +0100

    Finished the anecdotes exercises

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+const Randomize = ({handleClick}) => (
+  
+  <button onClick={handleClick}> Next anecdote </button>
+  
+)
+const MostVotes = ({text}) => (
+  <> 
+  <h1> The anecdote with most votes : </h1>
+  {text}
+  </>
+)
+
+const Vote = ({handleClick, text}) => (
+  <>
+  This anecdotes has : {text} votes 
+  <br/>
+  <button onClick={handleClick}> Vote </button> 
+  </>
+)
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  
+  let points = Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0);
+  
+  const [votes, setVotes] = useState(points)
+  
+  const nextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * 6)); 
+  }
+  
+  const vote = (selected) => {
+    const copy = [...votes];
+    copy[selected]+=1;
+    
+  
+    setVotes(copy);
+  
+  }
+  let max = votes.indexOf(Math.max(...votes));
+  
+  return (
+    <div>
+      <h1> Anecdote of the day </h1>
+      <strong > {props.anecdotes[selected]}. </strong>
+      <br/>
+      <Vote handleClick= {() => vote(selected)} text={votes[selected]}/> 
+      <Randomize handleClick={() => nextAnecdote()} />
+      <MostVotes text = {props.anecdotes[max]}/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
